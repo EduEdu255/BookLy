5d760d616fa03f243158d0f546cccebf410816d7
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-03-25 18:00:38 -0300

    feat: Adicionar e vizualisar livros da biblioteca do usuário autenticado

--- Back-end/app/Http/Controllers/BookController.php
+++ Back-end/app/Http/Controllers/BookController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return response()->json($request->user()->books);
+    }
+
+    public function show(Request $request, int $id)
+    {
+        if (!$book = $request->user()->books()->find($id)) {
+            return response()->json(['message' => 'Livro não encontrado'], 404);
+        }
+
+        return response()->json($book);
+    }
+
+    public function store(Request $request)
+    {
+        $new_book = $request->validate([
+            'external_id' => 'required|int',
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'author' => 'required|string',
+            'published_at' => 'required|date',
+        ]);
+
+        Book::create($new_book);
+
+        return response()->json([
+            'message' => 'Livro adicionado com sucesso',
+            'book' => $new_book
+        ], 201);
+    }
 }
