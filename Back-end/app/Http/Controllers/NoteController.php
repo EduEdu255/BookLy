2e3ea751a8210d80679dbfa4aba3aac940683d3b
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-04-07 08:48:35 -0300

    feat: index, show e delete de anotações & delete de usuarios

--- Back-end/app/Http/Controllers/NoteController.php
+++ Back-end/app/Http/Controllers/NoteController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function index(Request $request, Book $book)
+    {
+        return response()->json(['notes' => $book->notes]);
+    }
+
+    public function show(Request $request, int $id)
+    {
+        if(!$note = Note::find($id)){
+            return response()->json(['message' => 'Anotação não encontrada'], 404);
+        }
+
+        return response()->json(['note' => $note]);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function delete(Request $request, int $id)
+    {
+        if(!$note = Note::find($id)){
+            return response()->json(['message' => 'Anotação não encontrada'], 404);
+        }
+
+        $note->delete();
+
+        return response()->json(['message' => 'Anotação deletada com sucesso']);
+    }
 }
--- Back-end/app/Http/Controllers/UserController.php
+++ Back-end/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,5 +10,17 @@ class UserController extends Controller
     public function show(Request $request)
     {
         return response()->json($request->user());
+    }
+
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    public function delete(Request $request)
+    {
+        $request->user()->delete();
+
+        return response()->json(['message' => 'Usuário deletado com sucesso']);
     }
 }
