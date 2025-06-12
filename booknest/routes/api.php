f222511dc8a07c54900c9e5500cc0d3b759199ff
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-06-11 22:56:31 -0300

    fix: mudanças nas rotas de api

--- booknest/routes/api.php
+++ booknest/routes/api.php
@@ -9,8 +9,9 @@ Route::get('/', fn() => response()->json(['Hello World, BookNest API!']));
 Route::post('/register', [ApiAuthController::class, 'register']);
 Route::post('/login', [ApiAuthController::class, 'login']);
 
+Route::get('/books/search', [ApiBookController::class, 'search']); // quando submeter o form de pesquisa
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/books/search', [ApiBookController::class, 'search']); // quando submeter o form de pesquisa
     Route::get('/books', [ApiBookController::class, 'index']); // listar livros da biblioteca do usuário
     Route::get('/books/{external_id}', [ApiBookController::class, 'info']); // informações de um livro
     Route::post('/books', [ApiBookController::class, 'addToLib']); // adicionar livro à biblioteca
